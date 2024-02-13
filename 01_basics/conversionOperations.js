710e036294ad0f45601cdfe2e46e9910dc3492de
shub153 <159947319+shub153@users.noreply.github.com>
2024-02-13 20:05:06 +0000

    operation

---
+++ 01_basics/conversionOperations.js
@@ -0,0 +1,23 @@
+let score ="33abc"
+
+console.log(typeof score)
+
+
+let scoreInNumber = Number(score)
+console.log(typeof scoreInNumber);
+console.log( scoreInNumber);
+
+// "332" =>33
+// "332abs" =>NaN
+
+
+let isLoggedIn = 1
+
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+let someNumber=33
+let stringNumber=String(someNumber)
+console.log(stringNumber)
+console.log( typeof stringNumber)
+
