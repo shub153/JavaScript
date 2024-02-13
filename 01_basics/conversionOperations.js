dbbdd7467b4740e4557bb6a810b043108694d7ed
shub153 <159947319+shub153@users.noreply.github.com>
2024-02-13 20:22:17 +0000

    learn

--- 01_basics/conversionOperations.js
+++ 01_basics/conversionOperations.js
@@ -21,3 +21,36 @@ let stringNumber=String(someNumber)
 console.log(stringNumber)
 console.log( typeof stringNumber)
 
+// ************OPERATIONS************
+
+
+let value =3
+let negValue=-value
+//console.log(negValue)
+
+
+let str1="heelo"
+let str2= " shubham"
+
+let str3= str1+str2
+console.log(str3)
+
+
+console.log("1"+2)
+console.log(1+"2")
+
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+
+
+console.log(true)
+console.log(+true)
+
+
+let num1=num2=num3=2+2
+
+
+let gameCounter=100
+gameCounter++
+console.log(gameCounter)
--- 01_basics/variables.js
+++ 01_basics/variables.js
@@ -1,5 +1,19 @@
+<<<<<<< HEAD
+const accountId = 144553
+let accountEmail = "shubham"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+=======
 const accountId = 144553 // constant declaration
+>>>>>>> 710e036294ad0f45601cdfe2e46e9910dc3492de
 
+<<<<<<< HEAD
+
+
+accountId = 2
+
+console.log(accountId);
+=======
 let accountEmail = "shubham"  // varaiable declaration
 var accountPassword = "12345"  // variable declraration
 accountCity = "Jaipur"
@@ -26,3 +40,4 @@ accountCity ="pune"
 console.log(accountId);
 console.log(accountEmail);
 console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+>>>>>>> 710e036294ad0f45601cdfe2e46e9910dc3492de
