4d6558e97a05ac81ad4f55fea018febd6b2e1f3d
shub153 <159947319+shub153@users.noreply.github.com>
2024-02-13 19:39:40 +0000

    DATATYPE

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,29 @@
+"use strict";  // treat all JS code as newer version
+
+//alert(3+3) // we r using node js not browser
+
+console.log(3+3)
+console.log("shubham")
+
+
+let name = "hitesh" //string
+let age =18         //number
+let isLoggedIn=false //bool
+let state;
+
+//number =>2 to the power 53
+//bigint
+//string
+//boolean
+//null =>standalone value
+//undefined
+//symbol=> unique
+
+
+//object
+
+
+console.log(typeof("hitesh"))
+console.log(typeof(age))
+console.log(typeof(null))
+console.log(typeof(undefined))
---
+++ 01_basics/variables.js
@@ -0,0 +1,28 @@
+const accountId = 144553 // constant declaration
+
+let accountEmail = "shubham"  // varaiable declaration
+var accountPassword = "12345"  // variable declraration
+accountCity = "Jaipur"
+ let accountState;
+
+
+
+
+/*
+Prefrer not to use var
+bcoz of issue in block scope  and functional scope
+
+
+*/
+
+
+//accountId = 2 not allowed to changed constants
+
+
+accountEmail="shub"
+accountPassword="22122"
+accountCity ="pune"
+
+console.log(accountId);
+console.log(accountEmail);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
